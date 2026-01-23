9f1aa77d531113793c360d703fdc91b18de17a2c
TonicBox <gsec.tonicbox@protonmail.com>
2026-01-23 01:41:03 -0600

    - cleanup & bump

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,2 @@
 rootProject.name = "JStudio"
 
-includeBuild("../YABR-fixed") {
-    dependencySubstitution {
-        substitute(module("com.github.Tonic-Box:YABR")).using(project(":"))
-    }
-}
-
